2ac002b04b80e669d896041d3a272c233d76a44a
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-12-19 16:16:21 +0000

    Add project and assets

---
+++ JP Apex Predators/JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by José João Pimenta Oliveira on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
