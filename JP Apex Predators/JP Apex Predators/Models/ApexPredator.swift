d0870dc8eea7f189758b598565e4dc2327a1f4a6
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-12-19 16:33:56 +0000

    Added models
    ApexPredator and MovieScene

---
+++ JP Apex Predators/JP Apex Predators/Models/ApexPredator.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredator.swift
+//  JP Apex Predators
+//
+//  Created by José João Pimenta Oliveira on 19/12/2023.
+//
+
+import Foundation
+
+struct ApexPredator: Codable {
+    let id: Int
+    let name: String
+    let type: String
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+}
---
+++ JP Apex Predators/JP Apex Predators/Models/MovieScene.swift
@@ -0,0 +1,14 @@
+//
+//  MovieScene.swift
+//  JP Apex Predators
+//
+//  Created by José João Pimenta Oliveira on 19/12/2023.
+//
+
+import Foundation
+
+struct MovieScene: Codable {
+    let id: Int
+    let movie: String
+    let sceneDescription: String
+}
